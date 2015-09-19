20fea266f646d4122597062e73e51d06bc07908d
Lee Liu <lee@lee.liu>
2015-09-19 04:55:59 -0700

    authentication completed

--- index.js
+++ index.js
@@ -3,12 +3,26 @@
 var program = require('commander');
 var pkg = require('./package.json');
 var fs = require('fs');
-var Tail = require('tail').Tail;
+var Tail = require('./tail').Tail;
 var properties = require("properties");
 var _ = require("lodash");
 var io = require('socket.io-client');
+var request = require("request");
+var os = require("os");
+var macaddress = require('macaddress');
 
 var socket;
+var auth_token;
+var monitoring = false;
+
+var DEFAULT_CONF_FILE = "/etc/logdna.conf";
+var LOGDNA_APIHOST = process.env.LDAPIHOST || "api.logdna.com";
+var LOGDNA_APISSL = isNaN(process.env.USESSL) ? true : +process.env.USESSL;
+var LOGDNA_LOGHOST = process.env.LDLOGHOST;
+var LOGDNA_LOGPORT = process.env.LDLOGPORT;
+var LOGDNA_LOGSSL = process.env.LDLOGSSL;
+var AUTHFAIL_DELAY = 3600; // 1 hr
+// var AUTHFAIL_DELAY = 10; // 10s
 
 process.title = 'logdna-agent-linux';
 program._name = 'logdna-agent-linux';
@@ -33,7 +47,7 @@ if (process.getuid() > 0) {
     process.exit();
 }
 
-properties.parse(program.config || "/etc/logdna.conf", {
+properties.parse(program.config || DEFAULT_CONF_FILE, {
     path: true
 }, function(error, config) {
     config = config || {};
@@ -76,33 +90,94 @@ properties.parse(program.config || "/etc/logdna.conf", {
         return;
     }
 
-    socket = io('ws://logs.logdna.com', {
-        transports: ['websocket']
+    macaddress.all(function (err, all) {
+        var ifaces = [ 'eth0', 'eth1', 'en0', 'en1' ];
+        var mac, ip;
+        for (var i = 0; i < ifaces.length; i++) {
+            if (all[ifaces[i]]) {
+                mac = all[ifaces[i]].mac;
+                ip = all[ifaces[i]].ipv4 || all[ifaces[i]].ipv6;
+                break;
+            }
+        }
+        authenticate(config, mac, ip);
+    });
+});
+
+function authenticate(config, mac, ip) {
+    request.post( (LOGDNA_APISSL ? "https://" : "http://") + LOGDNA_APIHOST + "/authenticate/" + config.key, { json: { hostname: os.hostname(), mac: mac, ip: ip, agentname: program._name, agentversion: pkg.version } }, function(err, res, body) {
+        if (err || res.statusCode != "200") {
+            // got error, try again in an hour
+            if (err)
+                console.log("API error: " + err);
+            else
+                console.log("API error: " + res.statusCode + ": " + JSON.stringify(body));
+
+            return setTimeout(function() {
+                authenticate(config, mac, ip);
+            }, AUTHFAIL_DELAY * 1000);
+        }
+
+        console.log(body);
+
+        if (LOGDNA_LOGHOST || LOGDNA_LOGPORT || LOGDNA_LOGSSL) {
+            LOGDNA_LOGHOST = LOGDNA_LOGHOST || "logs.logdna.com";
+            LOGDNA_LOGPORT = LOGDNA_LOGPORT || 80;
+            LOGDNA_LOGSSL = LOGDNA_LOGSSL || false;
+
+        } else {
+            LOGDNA_LOGHOST = body.server;
+            LOGDNA_LOGPORT = body.port;
+            LOGDNA_LOGSSL = body.ssl;
+        }
+
+        auth_token = body.token;
+
+        if (!socket) {
+            setupSockets(config);
+        }
+    });
+}
+
+function setupSockets(config) {
+    console.log((LOGDNA_LOGSSL ? "wss://" : "ws://") + LOGDNA_LOGHOST + ":" + LOGDNA_LOGPORT);
+    socket = io( (LOGDNA_LOGSSL ? "wss://" : "ws://") + LOGDNA_LOGHOST + ":" + LOGDNA_LOGPORT, {
+        transports: ['websocket'],
+        query: { auth_token: auth_token }
     });
     socket.on('connect', function() {
-        console.log("connected to server.");
+        console.log("Connected to " + LOGDNA_LOGHOST + ".");
+
+        // monitor logs
+        if (!monitoring) {
+            _.each(config.logdir, function(dir) {
+                getDir(dir);
+            });
+        }
     });
     socket.on('error', function(err) {
-        console.log("error: " + err);
+        console.log("Server error: " + err);
+        if (~err.indexOf("401") || ~err.indexOf("403")) {
+            console.log("Got " + err.substring(0, 3) + " response, sleeping for " + AUTHFAIL_DELAY + "s...");
+            socket.io.reconnectionDelay(AUTHFAIL_DELAY * 1000);
+            socket.io.reconnectionDelayMax(AUTHFAIL_DELAY * 1000);
+        }
     });
     socket.on('disconnect', function() {
-        console.log("disconnected to server.");
+        console.log("Disconnected from server.");
     });
     socket.on('reconnecting', function(num) {
-        console.log("reconnecting to server: " + num);
+        console.log("Reconnecting to server: " + num);
+        socket.io.reconnectionDelay(1000); // reset
+        socket.io.reconnectionDelayMax(5000); // reset
     });
     socket.on('reconnect_error', function(err) {
-        console.log("reconnect error: " + err);
+        console.log("Reconnect error: " + err);
     });
     socket.on('event', function(data) {
         console.log(data);
     });
-
-    // monitor logs
-    _.each(config.logdir, function(dir) {
-        getDir(dir);
-    });
-});
+}
 
 function onNewLine(file) {
     return function(line) {
