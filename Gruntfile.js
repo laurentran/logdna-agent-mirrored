6142bea4a1fb1650fb83e84c08bac6c3180ce066
Lee Liu <lee@lee.liu>
2015-10-20 04:35:49 -0700

    use version from package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+var pkg = require('./package.json');
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -13,8 +14,8 @@ module.exports = function(grunt) {
         },
         exec: {
             nexe: "nexe -i index.js -o logdna-agent-linux -f -t ~/tmp -r 0.12.7",
-            fpm_rpm: 'fpm -s dir -t rpm -n logdna-agent-linux -v 1.0.0 --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent',
-            fpm_deb: 'fpm -s dir -t deb -n logdna-agent-linux -v 1.0.0 --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent'
+            fpm_rpm: 'fpm -s dir -t rpm -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent',
+            fpm_deb: 'fpm -s dir -t deb -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent'
         }
     });
     grunt.loadNpmTasks('grunt-line-remover');
