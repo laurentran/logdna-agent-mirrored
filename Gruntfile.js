af7911d1595208583f0da661e667f7a8d6c0cd06
Lee Liu <lee@lee.liu>
2015-11-03 00:03:38 -0800

    reorganize files into folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,8 +14,8 @@ module.exports = function(grunt) {
         },
         exec: {
             nexe: "nexe -i index.js -o logdna-agent-linux -f -t ~/tmp -r 0.12.7",
-            fpm_rpm: 'fpm -s dir -t rpm -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" --before-remove ./before-remove --after-upgrade ./after-upgrade -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent',
-            fpm_deb: 'fpm -s dir -t deb -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" --before-remove ./before-remove --after-upgrade ./after-upgrade -f --deb-no-default-config-files ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./init-script=/etc/init.d/logdna-agent'
+            fpm_rpm: 'fpm -s dir -t rpm -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" --before-remove ./scripts/before-remove --after-upgrade ./scripts/after-upgrade -f ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./scripts/init-script=/etc/init.d/logdna-agent',
+            fpm_deb: 'fpm -s dir -t deb -n logdna-agent-linux -v ' + pkg.version + ' --license Commercial --vendor "Answerbook, Inc." --description "LogDNA Agent for Linux" --url http://logdna.com/ -m "<help@logdna.com>" --before-remove ./scripts/before-remove --after-upgrade ./scripts/after-upgrade -f --deb-no-default-config-files ./logdna-agent-linux=/usr/bin/logdna-agent-linux ./scripts/init-script=/etc/init.d/logdna-agent'
         }
     });
     grunt.loadNpmTasks('grunt-line-remover');
